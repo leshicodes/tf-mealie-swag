b5d928d0b9e4844d3559e1ae0afbc66e3ba21973
James Ambrose <galaxy2198@gmail.com>
2023-09-29 15:41:32 -0500

    script addition

---
+++ Start-TerraformAction.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)][ValidateSet("plan", "apply", "destroy")][String]$TerraformAction,    
+    [Parameter(Mandatory = $false)][String]$TranscriptName
+)
+BEGIN {
+    # Sets the Transcript Name if t
+    if ([string]::IsNullOrEmpty($TranscriptName)) {
+        $TranscriptName = "Start-TerraformAction.$(Get-Date -Format "MM-dd-yyyy").log"
+    }
+    # Stop-Transcript -ErrorAction SilentlyContinue
+    Start-Transcript -Path ./$TranscriptName -Append
+    Push-Location $PSScriptRoot
+
+    $tfPlanName = "mealie-swag.plan"
+}
+PROCESS {
+    switch ($TerraformAction) {
+        "plan" {
+            terraform init
+            terraform plan -var-file="variables.tfvars" -out "$tfPlanName"
+        }
+
+        "apply" {
+            terraform plan -var-file="variables.tfvars" -out "$tfPlanName"
+            terraform apply "$tfPlanName"
+            # terraform -chdir="$ProjectDirectory" plan -var-file="$ProjectVarFilename" -out "$ProjectName.plan"
+        }
+        "destroy" {
+            terraform destroy -var-file="variables.tfvars" -auto-approve
+        }
+        
+
+    }
+}
+END {
+    Stop-Transcript
+    Pop-Location
+}
+
+
+
